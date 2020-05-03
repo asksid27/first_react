94f3f6ffa09c2316f9f18c75e19f319dcbaec3e1
asksid27 <46064343+asksid27@users.noreply.github.com>
2020-05-03 15:03:33 +0530

    Updates

---
+++ precache-manifest.416a684fdb131ba24921267813f46e6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7830840a17bf5b936329f1d5f3527df6",
+    "url": "/first_react/index.html"
+  },
+  {
+    "revision": "1a9074ab084946f63dc1",
+    "url": "/first_react/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "358d47a50e71e1812318",
+    "url": "/first_react/static/css/main.73bdd1eb.chunk.css"
+  },
+  {
+    "revision": "1a9074ab084946f63dc1",
+    "url": "/first_react/static/js/2.1613727d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/first_react/static/js/2.1613727d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358d47a50e71e1812318",
+    "url": "/first_react/static/js/main.4d2621e1.chunk.js"
+  },
+  {
+    "revision": "ae7dcd45b053c01f42ae",
+    "url": "/first_react/static/js/runtime-main.dc1ebde0.js"
+  },
+  {
+    "revision": "4bba530025d8cb83631795d4cafbe85f",
+    "url": "/first_react/static/media/logo.4bba5300.jpg"
+  }
+]);
